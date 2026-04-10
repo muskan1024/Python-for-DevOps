8342a2dfe493acf6895fe90c57265b4dcf230c14
muskan1024 <shaikhmuskan1024@gmail.com>
2026-04-10 20:38:46 +0530

    Started python and day01 file added

---
+++ day01/practice/my_first_file.py
@@ -0,0 +1,25 @@
+'''
+Notes
+--------------------------------------------
+variable : A variable in Python is a named container used to store data that can be changed during program execution.
+--------------------------------------------
+scripts : A set of instructions
+--------------------------------------------
+constants: The value that is stored in variable is known as constants OR A constant in Python is a value that is meant to stay the same and not change while the program runs. 
+--------------------------------------------
+assignment operator: An assignment operator in Python is used to give a value to a variable (like =). "=" is also known as assignment operator 
+--------------------------------------------
+Arithmetic operator: In Python, arithmetic operators are symbols used to perform basic math operations like addition, subtraction, multiplication, and division.
+--------------------------------------------
+Operands and Operators: Operands are the values you use (like numbers or variables) 
+Operators (operations) are the symbols that perform actions on them
+for example in a+b
+a and b are operands and + is a operator
+--------------------------------------------
+'''
+
+print("Hello From Python")
+
+a = 2000
+b = 100
+print("The addition of a and b is:", a+b)
