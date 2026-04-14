02384ab3e8af5558ef5da02d698892cedc03a3f7
muskan1024 <shaikhmuskan1024@gmail.com>
2026-04-14 13:21:53 +0530

    List and their methods

---
+++ practice/collection.py
@@ -0,0 +1,12 @@
+list_of_fruits = ["apple", "mango", "banana", "watermelon", "jackfruit", "strawberry"]
+
+print(list_of_fruits)
+
+list_of_fruits.append("litchi")
+list_of_fruits.append("peach")
+print(list_of_fruits)
+
+list_of_fruits.insert(4, "pineapple")
+print(list_of_fruits)
+
+print(len(list_of_fruits))
