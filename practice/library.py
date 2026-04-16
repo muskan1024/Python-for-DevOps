4251c80e5373a2383d1a0b4f5f5d44cfc90f401f
muskan1024 <shaikhmuskan1024@gmail.com>
2026-04-16 13:23:01 +0530

    OS and System library

---
+++ practice/library.py
@@ -0,0 +1,5 @@
+import os
+
+print(os.system('df -h')) #linux commands
+print(os.system('dir')) #windows commands
+print(os.system('time')) #windows commands
--- practice/loops.py
+++ practice/loops.py
@@ -4,3 +4,9 @@ list_of_cloud = ["AWS", "Azure", "GCP", "Oracle", "Utho", "SalesForce", "Alibaba
 for cloud in list_of_cloud:
     print(cloud)
     print(" ")
+    
+for i in range(1, 11):
+    print(i)
+    
+for i in range(1, 12):
+    print("Hello Muskan")
