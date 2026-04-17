f81c01c51045605e3467c2f4320c5818857cd49a
muskan1024 <shaikhmuskan1024@gmail.com>
2026-04-17 12:56:59 +0530

    function to find maximum number

--- practice/functions.py
+++ practice/functions.py
@@ -20,3 +20,13 @@ def greet(name):
     print("Hello", name)
     
 greet(name)
+
+# -----------------------------------------------------------------------------------------------
+# Problem 1: Find the Maximum (Basic)
+# Write a function called find_maximum that takes two numbers as arguments and returns the larger of the two.
+
+def find_max(num1, num2):
+    maximum = max(num1, num2)
+    print(maximum)
+    # return max(num1, num2)
+find_max(122,9)
