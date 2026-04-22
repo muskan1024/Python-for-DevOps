681a41693c90873f3b340fdd3fbc1f6709f05232
muskan1024 <shaikhmuskan1024@gmail.com>
2026-04-22 21:13:12 +0530

    Updates in backup file

--- project/backup.py
+++ project/backup.py
@@ -5,7 +5,9 @@ import shutil
 def backup(source, destination):
     today = datetime.date.today()
     backup_file = os.path.join(destination, f"backup_{today}")
+    # .path.join(destination, f"backup_{today}.tar.gz")
     shutil.make_archive(backup_file, 'gztar', source )
+    # shutil.make_archive(backup_file.replace(".tar.gz", ""), 'gztar', source )
     
 source = "/mnt/d/DevOps/Python/practice"
 destination = "/mnt/d/DevOps/Python/project/backups"
